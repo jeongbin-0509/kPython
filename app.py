12a7c9de75301ceea848fe417a140c9955d09424
ahnjb <159648234+jeongbin-0509@users.noreply.github.com>
2025-12-14 15:32:13 +0900

    app.py수정

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template, send_file
-import os
 
 app = Flask(__name__)
 
-@app.get("/")
-def home():
+@app.route("/")
+def index():
     return render_template("index.html")
 
-@app.get("/hangle.py")
-def hangle_file():
-    return send_file(os.path.join(os.path.dirname(__file__), "hangle.py"))
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/hangle.py")
+def hangle():
+    return send_file("hangle.py")
